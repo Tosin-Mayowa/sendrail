6d31b7dae23be28e4352ac7147946620210292ec
Tosin Mayowa <toss800@gmail.com>
2023-02-03 12:33:47 +0100

    add button to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,22 @@
+
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import {Box,Button} from "@chakra-ui/react";
+
 const Home=()=>{
-    return <div>hello</div>
+    const navigate = useNavigate();
+    return (
+      <>
+        <Box>hello</Box>
+        <Button onClick={() => navigate("/dashboard")}>Goto Dashboard</Button>
+        <Button ml="10px" onClick={() => navigate("/sign-up")}>
+          Goto SignUp
+        </Button>
+        <Button ml="10px" onClick={() => navigate("/login")}>
+          Goto Sign in
+        </Button>
+      </>
+    );
 }
 
 export default Home;
