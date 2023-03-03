1757a6524707b60f751e3b752bd2fad33f841224
Tosin Mayowa <toss800@gmail.com>
2023-03-03 15:28:41 +0100

    WIP

---
+++ src/Component/Dashboard/Transactions.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import withLayout from '../../wrapper/withLayout'
+const Transactions=()=>{
+
+    return (
+       
+        <div>Transaction</div>
+       
+    )
+}
+
+export default withLayout(Transactions);
