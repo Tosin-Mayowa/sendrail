deae4238caa5bf3993af07402a7d8e8bcf510a23
Tosin Mayowa <toss800@gmail.com>
2023-04-30 17:09:19 +0100

    menu component

--- src/Component/Dashboard/ManageRider/TableBody.tsx
+++ src/Component/Dashboard/ManageRider/TableBody.tsx
@@ -19,6 +19,7 @@ Badge,
   Button
 } from '@chakra-ui/react';
 import Order from "./Order"
+import { MenuComponent } from './MenuComponent';
 const TableBody = ({ item, phn, msg, menu, setIndx }) => {
   return (
     <>
@@ -68,66 +69,7 @@ const TableBody = ({ item, phn, msg, menu, setIndx }) => {
           </Flex>
         </Td>
         <Td textAlign="center">
-          <Menu placement="bottom">
-            <MenuButton>
-              <Image src={menu} alt="menu" width="3.74px" height="16.56px" objectFit="cover" />
-            </MenuButton>
-
-            <MenuList
-              width="9.625rem"
-              filter="drop-shadow(2px 2px 20px rgba(105, 99, 186, 0.160784))"
-              height="18.375rem">
-              <MenuItem
-                height="3.0625rem"
-                padding="1rem"
-                borderRadius="10px 10px 0px 0px"
-                fontSize="14px"
-                color="text.300"
-                lineHeight="17px"
-                onClick={() => setIndx(1)}>
-                View profile
-              </MenuItem>
-              <MenuItem>
-                <Order item={item} phn={phn} msg={msg} />
-              </MenuItem>
-              <MenuItem
-                height="3.0625rem"
-                padding="1rem"
-                borderRadius="10px 10px 0px 0px"
-                fontSize="14px"
-                color="text.300"
-                lineHeight="17px">
-                Track courier
-              </MenuItem>
-              <MenuItem
-                height="3.0625rem"
-                padding="1rem"
-                borderRadius="10px 10px 0px 0px"
-                fontSize="14px"
-                color="text.300"
-                lineHeight="17px">
-                View
-              </MenuItem>
-              <MenuItem
-                height="3.0625rem"
-                padding="1rem"
-                borderRadius="10px 10px 0px 0px"
-                fontSize="14px"
-                color="text.300"
-                lineHeight="17px">
-                View payslip
-              </MenuItem>
-              <MenuItem
-                height="3.0625rem"
-                padding="1rem"
-                borderRadius="10px 10px 0px 0px"
-                fontSize="14px"
-                color="text.300"
-                lineHeight="17px">
-                Create payroll
-              </MenuItem>
-            </MenuList>
-          </Menu>
+          <MenuComponent setIndx={setIndx} phn={phn} msg={msg} item={item} menu={menu}/>
         </Td>
       </Tr>
     </>
